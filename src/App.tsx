b73fb7a76244b1e2bfd985f2f8c0cbd0feb399ad
SaintAM <mistersteepstar@gmail.com>
2023-09-15 15:16:06 +0300

    "pathName"

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
     return (
         <Routes>
             <Route path="/" element={<MainLayout />}>
-                <Route path="react-pizza" element={<Home />} />
+                <Route path="/react-pizza" element={<Home />} />
                 <Route
                     path="cart"
                     element={
